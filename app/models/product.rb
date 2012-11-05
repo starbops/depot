02fc46d78bda5dd97983fb47770fdcf712174959
Zespre Schmidt <starbops@gmail.com>
2012-11-05 13:12:13 +0800

    Add Validators

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  validates_presence_of :title, :description, :image_url
+  validates_numericality_of :price, :greater_than_or_equal_to => 0.01
+  validates_uniqueness_of :title
+  validates_format_of :image_url, :with => %r{\.(gif|jpg|png)$}i,
+                                  :message => 'must be a URL for GIF, JPG or PNG image.'
 end
