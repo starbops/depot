fd36449e624f51be8312e502232f0e299bc0285c
Zespre Schmidt <starbops@gmail.com>
2012-11-05 21:50:23 +0800

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
-  validates_presence_of :title, :description, :image_url
-  validates_numericality_of :price, :greater_than_or_equal_to => 0.01
-  validates_uniqueness_of :title
-  validates_format_of :image_url, :with => %r{\.(gif|jpg|png)$}i,
-                                  :message => 'must be a URL for GIF, JPG or PNG image.'
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {
+    :with => %r{\.(gif|jpg|png)$}i,
+    :message => 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
