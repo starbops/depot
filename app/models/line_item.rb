7cd4ab2028d45bbac5587709de002be2f17c0229
Zespre Schmidt <starbops@gmail.com>
2012-11-08 15:20:19 +0800

    A Smarter Cart Modifying the schema and existing data. Error handling, flash, and logging.

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,12 @@
 class LineItem < ActiveRecord::Base
-  #attr_accessor :product
-  attr_accessible :cart_id, :product_id, :product
+  # attr_accessor :product
+  attr_accessible :cart_id, :product_id, :product, :quantity
 
   belongs_to :product
   belongs_to :cart
+
+  def total_price
+    product.price * quantity
+  end
+
 end
