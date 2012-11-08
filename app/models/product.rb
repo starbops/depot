e1c34e151db82f0fa3c5c957e61c0bdeea662368
Zespre Schmidt <starbops@gmail.com>
2012-11-08 21:24:30 +0800

    A few refinement

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,8 +9,7 @@ class Product < ActiveRecord::Base
   # validation stuff
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
-  validates :title, :uniqueness => true,
-                    :length => {:minimum => 10}
+  validates :title, :uniqueness => true
   validates :image_url, :format => {
                           :with => %r{\.(gif|jpg|png)$}i,
                           :message => 'must be a URL for GIF, JPG or PNG image.'
@@ -18,7 +17,7 @@ class Product < ActiveRecord::Base
 
   private
 
-    # ensure that there are no line items referencing this product
+  # ensure that there are no line items referencing this product
   def ensure_not_referenced_by_any_line_item
     if line_items.empty?
       return true
--- db/seeds.rb
+++ db/seeds.rb
@@ -40,7 +40,6 @@ Product.create(:title => 'Programming Ruby 1.9',
   :image_url => '/images/ruby.jpg',
   :price => 49.50)
 # . . .
-
 Product.create(:title => 'Rails Test Prescriptions',
   :description => 
     %{<p>
@@ -53,4 +52,15 @@ Product.create(:title => 'Rails Test Prescriptions',
       </p>},
   :image_url => '/images/rtp.jpg',
   :price => 43.75)
-
+# . . .
+Product.create(:title => 'Debug It!',
+  :description => 
+    %{<p>
+       Professional programmers develop a knack of unerringly zeroing in on
+       the root cause of a bug. They can do that because they've written
+       a lot of buggy code and then gained experience fixing it. This book
+       captures all this experience -- use it, and you'll find you write
+       fewer bugs, and the ones you do write will become easier to hunt down.
+      </p>},
+  :image_url => '/images/debug.jpg',
+  :price => 34.95)
